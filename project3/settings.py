82c60038f348bce30aa4e21ebaaf74986f93e5ab
DTCarlox77 <carlosadrianespinosaluna147@gmail.com>
2023-12-10 20:14:51 -0600

    Assh

--- project3/settings.py
+++ project3/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,7 @@ ROOT_URLCONF = 'project3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
