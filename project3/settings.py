53858b68419882dc3b469eb9bc2c541de3d4c491
DTCarlox77 <carlosadrianespinosaluna147@gmail.com>
2023-12-10 19:33:11 -0600

    True

--- project3/settings.py
+++ project3/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c7wp&=o#_)-s&4%)!p07r!#+5y&u2^ee%zy%zk3h76^yd#y=0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
